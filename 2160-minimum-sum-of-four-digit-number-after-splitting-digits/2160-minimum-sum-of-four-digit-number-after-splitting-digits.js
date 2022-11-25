7e272ef18b50ad089c5d00a6078e9a4535784407
Ngô Quang Đạt <66765392+ngodatq-26@users.noreply.github.com>
2022-11-26 05:41:06 +0700

    Time: 79 ms (75.61%), Space: 43.7 MB (5.61%) - LeetHub

---
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var minimumSum = function(num) {
+    var a = num.toString();
+    var arr = [];
+    for (let i = 0; i < a.length; i++) {
+        arr.push(a[i]);
+    }
+    arr.sort();
+    console.log(arr)
+    var num1 = arr[0] + arr[2];
+    var num2 = arr[1] + arr[3];
+    console.log(num1, num2)
+    return num1*1 + num2 *1;
+};
